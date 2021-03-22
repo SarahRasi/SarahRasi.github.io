2376f0fd6c3c603ae01c274a828b0696a131e026
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-03-22 16:51:23 +0100

    stage 2 create var

---
+++ codecademy/js_variables/main.js
@@ -0,0 +1,4 @@
+var favouriteFood = 'pizza'; //erstellt variable die unter favouriteFood abgespeichert ist
+var numOfSlices = 8; //gleich mit Zahl als value
+console.log(favouriteFood); //zeigt auf, was unter variable abgespeichert ist
+console.log(numOfSlices); 

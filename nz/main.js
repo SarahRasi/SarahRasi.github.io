9a31595522134b9308c96cc817172191cafb8cc6
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-03-25 18:04:48 +0100

    sort

--- nz/main.js
+++ nz/main.js
@@ -26,7 +26,7 @@ let nav = document.querySelector("#navigation");
 //console.log(nav);
 //console.log(ROUTE);
 ROUTE.sort((stop1, stop2) => {
-    return stop1.nr > stop2.nr //sortiert nach nummern aufsteigend
+    return stop1.nr > stop2.nr
 });
 
 

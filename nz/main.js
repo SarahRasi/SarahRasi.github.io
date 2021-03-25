310ff4a20f8ee15d56269708328b4f6c2bca8b27
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-03-25 17:20:31 +0100

    entry ROUTE

--- nz/main.js
+++ nz/main.js
@@ -21,11 +21,18 @@ const map = L.map("map", {
     ] 
 });
 
-let mrk = L.marker([ -39.4928444 , 176.9120178 ]).addTo(map);
-mrk.bindPopup(`
-    <h4>Stop ${stop.nr}: ${stop.name}</h4>
-    <p><i class="fas fa-external-link-alt mr-3"></i><a href="${stop.wikipedia}">Read about stop in Wikipedia</a></p>
-`).openPopup(); //`` 
+console.log(ROUTE);
+for(let entry of ROUTE) {
+    console.log(entry);
+
+    let mrk = L.marker([ entry.lat , entry.lng ]).addTo(map);
+    mrk.bindPopup(`
+        <h4>Stop ${entry.nr}: ${entry.name}</h4>
+        <p><i class="fas fa-external-link-alt mr-3"></i><a href="${entry.wikipedia}">Read about stop in Wikipedia</a></p>
+    `).openPopup(); //`` 
+}
+
+
 
 //console.log(document.querySelector("#map")); //# für css ID; Karte initialisiert und DIV in der Konsole angezeigt
 

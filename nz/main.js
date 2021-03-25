0c80a89d96e966e35c248b7918b329604ed8b454
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-03-25 16:14:21 +0100

    kartetileLayer

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,13 @@
 console.log("Hello World!");
 console.log(L); //L für leaflet
 
-const map = L.map("map");
-console.log(document.querySelector("#map"));
+const map = L.map("map", {
+    center: [-39.4928444 , 176.9120178 ],
+    zoom: 13
+    layers: [
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")  //s server, x longitude, y latitude
+    ]
+});
+
+console.log(document.querySelector("#map")); //# für css ID; Karte initialisiert und DIV in der Konsole angezeigt
+

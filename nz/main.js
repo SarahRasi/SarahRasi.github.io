6302fa8c482cb6ee09df0b9e7d3b3b0bca13ed9e
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-04-05 18:26:35 +0200

    mehr anpassen

--- nz/main.js
+++ nz/main.js
@@ -40,17 +40,13 @@ ROUTE.sort((stop1, stop2) => {
 
 
 for(let entry of ROUTE) {
-   // console.log(entry);
+   // console.log(entry); 
 
-
-    nav.innerHTML += `
-        <option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>
-    `;
-    let mrk = L.marker([ entry.lat , entry.lng ]).addTo(map);
-    mrk.bindPopup(`
-        <h4>Stop ${entry.nr}: ${entry.name}</h4>
-        <p><i class="fas fa-external-link-alt mr-3"></i><a href="${entry.wikipedia}">Read about stop in Wikipedia</a></p>
-    `); //`` 
+    nav.innerHTML += `<option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>`;
+    let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
+    mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
+    <p><a href="${entry.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
+`);
 
     if (entry.nr == 21) {
         map.setView([entry.lat, entry.lng], 13);

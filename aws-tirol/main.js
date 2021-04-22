0fc04febec37f6be91da6f1b4af7951416e9bc46
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-04-22 15:59:15 +0200

    overlays

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -69,6 +69,27 @@ fetch(awsUrl)
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
 
+            marker.addTo(overlays.stations);
+            if (station.properties.HS) {
+                let highlightClass = '';
+                if (station.properties.HS > 100) {
+                    highlightClass = 'snow-100';
+                }
+                if (station.properties.HS > 200) {
+                    highlightClass = 'snow-200';
+                }
+                let snowIcon = L.divIcon({
+                    html: `<div class="snow-label ${highlightClass}">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(overlays.snowheight);
+            }
+
             marker.addTo(overlays.windspeed);
             if (station.properties.WG) {
                 let windHighlightClass = '';

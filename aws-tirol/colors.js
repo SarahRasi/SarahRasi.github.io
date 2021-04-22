0847f12d64538d5024b61b788f067db3f0307e5e
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-04-22 18:13:06 +0200

    werte an Enden vergrößert

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -29,7 +29,7 @@ const COLORS = {
         col: "#ff3232"
     }],
     temperature: [{
-        min: -40,
+        min: -99,
         max: -25,
         col: "#9f80ff"
     }, {
@@ -78,7 +78,7 @@ const COLORS = {
         col: "#ff1e23"
     }, {
         min: 30,
-        max: 50,
+        max: 99,
         col: "#fa3c96"
     }],
     snowheight: [{

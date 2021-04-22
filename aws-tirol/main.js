495b0e14de4f5a9fffa4d15d5f6206501d93dc5b
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-04-22 17:57:49 +0200

    colors versuchen einzubinden

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -47,7 +47,13 @@ L.control.scale({
     imperial: false
 }).addTo(map);
 
+
+let getColor = (value, colorRamp) => {
+
+};
+
 let newLabel = (coords, options) => {
+    let color = getColor(options.value, options.colors)
     let label = L.divIcon({
         html: `<div>${options.value}</div>`,
         className: "text-label"
@@ -90,7 +96,8 @@ fetch(awsUrl)
             marker.addTo(overlays.stations);
             if (typeof station.properties.HS == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
-                    value: station.properties.HS
+                    value: station.properties.HS,
+                    colors: COLORS.snowheight
                 });
                 marker.addTo(overlays.snowheight);
 
@@ -98,7 +105,8 @@ fetch(awsUrl)
             marker.addTo(overlays.windspeed);
             if (typeof station.properties.WG == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
-                    value: station.properties.WG
+                    value: station.properties.WG,
+                    colors: COLORS.windspeed
                 });
                 marker.addTo(overlays.windspeed);
         
@@ -106,7 +114,8 @@ fetch(awsUrl)
             marker.addTo(overlays.temperature);
             if (typeof station.properties.LT == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
-                    value: station.properties.LT
+                    value: station.properties.LT,
+                    colors: COLORS.temperature
                 });
                 marker.addTo(overlays.temperature);
             } 

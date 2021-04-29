47d882c4d1fff22591eed223bd41cd6bd7fc0cd3
SarahRasi <sarah.rasi@student.uibk.ac.at>
2021-04-29 11:43:40 +0200

    rainviewer in index.html und main js eingebaut

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -50,6 +50,16 @@ L.control.scale({
     imperial: false
 }).addTo(map);
 
+L.control.rainviewer({
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
 
 let getColor = (value, colorRamp) => {
     for (let rule of colorRamp) {
